9e7f1f816b6e7fee30ef1b7d8e8ef83ed862a390
Gastón Haro <harogaston@gmail.com>
2025-12-26 06:35:10 -0300

    custom finder patterns

--- writers/svg.go
+++ writers/svg.go
@@ -25,18 +25,17 @@ func WriteSVG(req SVGRequest) {
 	}
 	defer file.Close()
 
-	rows := len(req.Pixels)
-	cols := len(req.Pixels[0])
-	width := cols * req.Scale
-	height := rows * req.Scale
+	dim := len(req.Pixels)
+	size := dim * req.Scale
 	canvas := svg.New(file)
-	canvas.Start(width, height)
+	canvas.Start(size, size)
 
 	for y, row := range req.Pixels {
 		for x, c := range row {
 			switch req.Shape {
 			case ShapeCircle:
-				canvas.Circle(x*req.Scale+req.Scale/2, y*req.Scale+req.Scale/2, req.Scale/2, CircleStyle(c))
+				canvas.Circle(x*req.Scale+req.Scale/2, y*req.Scale+req.Scale/2, req.Scale/2, CircleStyle(c, 0))
+				// Superimpose finder patterns
 			case ShapeSquare:
 				canvas.Square(x*req.Scale, y*req.Scale, req.Scale, SquareStyle(c))
 			case ShapeSlanted:
@@ -51,7 +50,37 @@ func WriteSVG(req SVGRequest) {
 			}
 		}
 	}
+	// Superimpose finder patterns
+	switch req.Shape {
+	case ShapeCircle:
+		CircleFinderPattern(canvas, 4, 4, req.Scale)
+		CircleFinderPattern(canvas, (dim - 7 - 4), 4, req.Scale)
+		CircleFinderPattern(canvas, 4, (dim - 7 - 4), req.Scale)
+	case ShapeSquare:
+	case ShapeSlanted:
+	}
 	canvas.End()
+}
+
+func CircleFinderPattern(canvas *svg.SVG, x, y int, scale int) {
+	canvas.Square(x*scale, y*scale, 8*scale, SquareStyle(color.White))
+	canvas.Circle(
+		int((float32(x)+float32(3.5))*float32(scale)),
+		int((float32(y)+float32(3.5))*float32(scale)),
+		int(float32(3.5)*float32(scale)), CircleStyle(color.Black, 4),
+	)
+	canvas.Circle(
+		int((float32(x)+float32(3.5))*float32(scale)),
+		int((float32(y)+float32(3.5))*float32(scale)),
+		int(float32(2.5)*float32(scale)), CircleStyle(color.White, 4),
+	)
+	canvas.Circle(
+		int((float32(x)+float32(3.5))*float32(scale)),
+		int((float32(y)+float32(3.5))*float32(scale)),
+		int(float32(1.5)*float32(scale)), CircleStyle(color.Black, 4),
+	)
+	// canvas.Circle((x+3.5)*scale, (y+3.5)*scale, int(float32(2.5)*float32(scale)), CircleStyle(color.White, 4))
+	// canvas.Circle((x+3.5)*scale, (y+3.5)*scale, int(float32(1.5)*float32(scale)), CircleStyle(color.Black, 4))
 }
 
 func SquareStyle(c color.Color) string {
@@ -61,23 +90,12 @@ func SquareStyle(c color.Color) string {
 	return "fill:white;stroke:none"
 }
 
-func CircleStyle(c color.Color) string {
-	if c == color.Black {
-		return "fill:black;stroke:white;stroke-width:2"
+func CircleStyle(c color.Color, border int) string {
+	if border <= 0 {
+		border = 2
 	}
-	return "fill:white;stroke:white;stroke-width:2"
+	if c == color.Black {
+		return fmt.Sprintf("fill:black;stroke:white;stroke-width:%d", border)
+	}
+	return fmt.Sprintf("fill:white;stroke:white;stroke-width:%d", border)
 }
-
-// _basicDot(args: BasicFigureDrawArgs): void {
-//   const { size, x, y } = args;
-//
-//   this._rotateFigure({
-//     ...args,
-//     draw: () => {
-//       this._element = this._window.document.createElementNS("http://www.w3.org/2000/svg", "circle");
-//       this._element.setAttribute("cx", String(x + size / 2));
-//       this._element.setAttribute("cy", String(y + size / 2));
-//       this._element.setAttribute("r", String(size / 2));
-//     }
-//   });
-// }
